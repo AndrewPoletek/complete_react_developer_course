a1cf2b735f8e83098215b4c86fe6d02b0fac1f0b
Andrzej Połetek <andrzej@poletek.net>
2023-11-29 15:08:24 +0100

    [2023-11-29] training unit

--- clothing/src/routes/categories-preview/categories-preview.component.jsx
+++ clothing/src/routes/categories-preview/categories-preview.component.jsx
@@ -1,10 +1,9 @@
-import { useContext } from "react"
-import { CategoriesContext } from "../../context/categories.context"
 import CategoryPreview from "../../components/category-preview/category-preview.component"
+import {selectCategoriesMap} from '../../store/categories/category.selector'
+import {useSelector } from "react-redux/es/hooks/useSelector"
 
 const CategoriesPreview = () =>{
-
-    const {categoriesMap} = useContext(CategoriesContext)
+    const categoriesMap = useSelector(selectCategoriesMap)
 
     return(
         <>
--- clothing/src/routes/category/category.component.jsx
+++ clothing/src/routes/category/category.component.jsx
@@ -1,16 +1,18 @@
 import './category.styles.scss'
 import { useParams } from 'react-router-dom'
-import { useContext, useState, useEffect } from 'react'
-import { CategoriesContext } from '../../context/categories.context'
+import {useState, useEffect } from 'react'
 import ProductCard from '../../components/product-card/product-card.component'
+import {selectCategoriesMap} from '../../store/categories/category.selector'
+import { useSelector } from "react-redux/es/hooks/useSelector"
 
 const Category = () =>{
     const {category} = useParams()
-    const {categoriesMap} = useContext(CategoriesContext)
+    const categoriesMap = useSelector(selectCategoriesMap)
     const [products, setProducts] = useState([])
 
 
     useEffect(()=>{
+        console.log(categoriesMap)
         setProducts(categoriesMap[category])
     }, [category, categoriesMap])
 
